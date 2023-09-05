8c8df17bf9b1e1af7124639f65ad9a21ff474ac9
mostafiz-zaag <mostafizur@zaagsys.com>
2023-09-05 14:45:11 +0600

    various identifier

---
+++ idendifier.js
@@ -0,0 +1,19 @@
+// variable declaration
+const userName = "mostafiz";
+
+// function declaration old
+function name (){
+    // code
+}
+
+// function declaration new 
+const name = () =>{
+    // code 
+}
+
+// class declaration 
+class name {
+    #priv = "value"; // private propperty
+}
+
+label1 : console.log("This is label");
