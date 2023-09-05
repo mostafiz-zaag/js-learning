0ed75cb3c7039362af52cce5733d6942e4b558f0
mostafiz-zaag <mostafizur@zaagsys.com>
2023-09-05 15:48:12 +0600

    literal.js modified

---
+++ Lexical-struture/literal.js
@@ -0,0 +1,54 @@
+// null literaals
+null 
+
+// boolean 
+true , false
+
+// numeric 
+Number, BigInt
+
+// exponential like
+5e1 // 50
+
+// binary
+//0101010101
+
+// octal
+// 0075
+
+// hexadecimal : 0xFFF
+
+
+// bigInt Cannot start with zero 
+// BigInt : 1243536272677n
+
+
+// numeric separator
+
+1_00_000_000_000
+
+
+// Regular expression 
+/ab+c/g,
+/[/]/
+
+// tamplate literals
+`some test ${expression} some text`
+
+// autometic semicolon intertion 
+
+/* 
+var, let, const
+Expression statements
+do...while
+continue, break, return, throw
+debugger
+Class field declarations (public or private)
+import, export
+
+*/
+
+
+
+
+
